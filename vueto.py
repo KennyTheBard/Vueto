0a66326b83212902c9d60f4b19f9712936a0face
KennyTheBard <octavian.teculescu@stud.acs.upb.ro>
2019-04-20 14:11:14 +0300

    The new random name generator is working! Support available only for order 1

--- vueto.py
+++ vueto.py
@@ -5,39 +5,37 @@ import loader
 
 # Commands
 EXIT = "exit"
-LIST = "list"
 LOAD = "load"
 HELP = "help"
 GENERATE = "gen"
-DEFINE = "def"
 
-sets = loader.read_sets()
-
-words = []
-constants = {"STD_MAX": 5}
+set = {}
 cmd = ""
 
 def help():
 	print LOAD + " <word_set> <order>\tload the requested load set"
-	print GENERATE + " <max_len>\tcreate a random word out of the previous loaded\n\t\tword set. A word set has to be already loaded.\n\t\tA maximum word length must be selected."
+	print GENERATE + " (<repets>)\tcreate a random word out of the previous loaded\n\t\tword set. A word set has to be already loaded.\n\t\tA maximum word length must be selected."
 	print EXIT + "\t\tclose the program"
 
 
-def load(set):
-	global words
-	(words, msg) = loader.load(set)
+def load(setname, order):
+	global set
+	(set, msg) = loader.load(setname, order)
 	print msg
 
 
-def generate(max_length):
-	if len(words) == 0:
+def generate():
+	if len(set) == 0:
 		print "ERROR: No set was loaded!"
 		return
 
-	wordcount = rand.randint(2, max_length)
 	new_word = ""
-	for i in range(wordcount):
-		new_word += rand.choice(words)
+	curr = rand.choice(set.keys())
+	while True:
+		new_word += curr
+		curr = rand.choice(set[curr])
+		if curr == "":
+			break
 
 	print new_word
 
@@ -52,28 +50,23 @@ while True:
 	cmd = interogate_user().lower().strip().split(" ")
 	if EXIT in cmd:
 		break
+
 	elif HELP in cmd:
 		help()
 
-	elif LIST in cmd:
-		list()
-
 	elif LOAD in cmd:
-		load(cmd[1])
 		if len(cmd) > 2:
 			load(cmd[1], int(cmd[2]))
 		else:
-			load(cmd[1], rand.randint(2, 12))
+			help()
+
 	elif GENERATE in cmd:
+		repets = 1
 		if len(cmd) > 1:
-			generate(int(cmd[1]))
-		else:
-			generate(constants["STD_MAX"])
-	elif DEFINE in cmd:
-		if len(cmd) > 2:
-			define(cmd[1], int(cmd[2]))
-		else:
-			print
+			repets = int(cmd[1])
+		for i in xrange(repets):
+			generate()
+
 	else:
 		print "Use <" + HELP + "> for details about how to use the program"
 	print ""
